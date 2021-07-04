b37c685bb3d4c0fe8b7834151a008033deada388
[crsgustav0] <[cristian.dias@terraviva.agr.br]>
2021-07-03 22:12:57 -0300

    Criação segunda area de trabalho

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade;
+            idade = 23;
+
+            Console.WriteLine("Aplicação .NET - NetDesktop");
+
+            Console.WriteLine("Execução sendo iniciada...");
+            Console.WriteLine(idade);
+            Console.WriteLine("A execução acabou. Tecle Enter para finalizar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
