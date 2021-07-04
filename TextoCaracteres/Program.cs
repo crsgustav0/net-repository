688aaa29bc5eba13fc21788c5d4ad51346953d30
[crsgustav0] <[cristian.dias@terraviva.agr.br]>
2021-07-03 22:44:09 -0300

    CaracteresTexto

---
+++ TextoCaracteres/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TextoCaracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando TextoCaracteres...");
+
+            char primeiraLetra = 'a';
+            finalizarExecucao(primeiraLetra);
+        }
+
+        private static void finalizarExecucao(char conteudo)
+        {
+            Console.WriteLine(conteudo);
+            Console.WriteLine("A execução acabou. Tecle Enter para finalizar... ");
+            Console.ReadLine();
+        }
+    }
+}
