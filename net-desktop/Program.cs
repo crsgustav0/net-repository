806e771af3c23a4aaa9341e4fbaeb2c5e403f5ac
[crsgustav0] <[cristian.dias@terraviva.agr.br]>
2021-07-03 21:43:37 -0300

    Adicionar arquivos de projeto.

---
+++ net-desktop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo");
+        }
+    }
+}
